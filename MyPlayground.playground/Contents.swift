13fc113347b37906f50ae119d6805f9904fd0a52
sravanpabolu <sravan@Sravans-MacBook-Pro.local>
2016-09-24 14:01:14 +0530

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Sravan"
+
+let status = 404
+let statusCode:Int? = 400
+
+print("statusCode value is : \(statusCode!)")
+
+//(1, "abcd") < (2, "bbbb")
+//(1, "zebra") < (2, "apple")
+//(4, "dog") == (4, "dog")      // true because 4 is equal to 4, and "dog" is equal to "dog"
